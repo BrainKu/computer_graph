413f3a2e407c51c13a4f1ca04ef41da1b3faa1be
kuxinwei <kuxinwei@163.com>
2014-05-24 20:15:28 +0800

    重构之前

--- ComputerGraphic/SomePlanet.h
+++ ComputerGraphic/SomePlanet.h
@@ -21,18 +21,21 @@ public:
 	};
 	void draw(float x, float y, float z) {
 		glMatrixMode(GL_MODELVIEW);
-		glPushMatrix();
-		glTranslatef(x, y, z);
 		std::vector<SolidSphere>::iterator i;
 		int count = 1;
-		glRotatef(count, 0.0, 1.0, 0.0);
-		for ( i = planets.begin(); i != planets.end();i++) {
-			i->draw(8 * count, 1 , 10);
+		//glRotatef(count, 0.0, 1.0, 0.0);
+		for (i = planets.begin(); i != planets.end(); i++) {
+			glPushMatrix();
+			glTranslatef(x, y, z);
+			glRotatef(Angle / count, 0, 1, 0);
+			std::cout << "ANGLE:" << Angle << std::endl;
+
+			i->draw(8 * count, 1, 10);
 			count++;
+			glPopMatrix();
 		}
 
 		Angle += 1;
-		glPopMatrix();
 	};
 private:
 	std::vector<SolidSphere> planets;
